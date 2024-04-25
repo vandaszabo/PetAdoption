9885b758b4b392cf91fe9582dbcfbdd74b239959
vanda.szabo <msvandaszabo@gmail.com>
2024-04-25 13:16:15 +0200

    remove: Delete quiz classes

--- PetAdoptionAPI/src/main/java/com/codecool/petadoption/repositories/QuestionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codecool.petadoption.repositories;
-
-import com.codecool.petadoption.models.QuizQuestion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRepository extends JpaRepository<QuizQuestion, Long> {
-
-}
