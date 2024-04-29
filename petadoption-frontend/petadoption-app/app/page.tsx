dbe46c0cc5e6d153f1143a7b7fa3f17e4ba70aa0
vanda.szabo <msvandaszabo@gmail.com>
2024-04-29 19:03:02 +0200

    style: Add action buttons

--- petadoption-frontend/petadoption-app/app/page.tsx
+++ petadoption-frontend/petadoption-app/app/page.tsx
@@ -1,19 +1,29 @@
+import ActionCard from "@/components/ActionCard";
 import GetStarted from "@/components/GetStarted";
+import petShopImage from '../public/pet-shop.png';
+import petOwner from '../public/owner.png';
+import petsAllowed from '../public/pets-allowed.png';
+
 
 export default function Home() {
-  return (
-    <>
-    <div className="wrapper">
-      <div className="left"></div>
-      <div className="right">
-      <h1>Find. <strong>Adopt</strong>. Love.</h1>
-      <GetStarted/>
-      </div>
-    </div>
-    <div className="advert">
-      <div className="advert-image"></div>
-      <p>Pawfect Pet Guarantee</p>
-    </div>
-    </>
-  );
+    return (
+        <main>
+            <div className="wrapper">
+                <div className="left"></div>
+                <div className="right">
+                    <h1>Find. <strong>Adopt</strong>. Love.</h1>
+                    <GetStarted />
+                </div>
+            </div>
+            <div className="advert">
+                <div className="advert-image"></div>
+                <span>Pawfect Pet Guarantee</span>
+            </div>
+            <div className='action-wrapper'>
+                <ActionCard text={'non-profit animal shelter'} image={petShopImage} />
+                <ActionCard text={'search by type, breed, location and more!'} image={petOwner} />
+                <ActionCard text={'news & updates on newest pets'} image={petsAllowed} />
+            </div>
+        </main>
+    );
 }
---
+++ petadoption-frontend/petadoption-app/components/ActionCard.tsx
@@ -0,0 +1,19 @@
+import { StaticImageData } from 'next/image';
+import React from 'react'
+import  Image from 'next/image';
+
+interface ActionCardProps {
+  text: string;
+  image: StaticImageData;
+}
+
+const ActionCard: React.FC<ActionCardProps> = ({ text, image }) => {
+  return (
+    <div className='action-btn-card'>
+        <Image src={image} alt={text}/>
+      <span>{text}</span>
+    </div>
+  )
+}
+
+export default ActionCard
