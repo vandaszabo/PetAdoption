7f7456735af966a759f6f28bb32684f732d5f04b
vanda.szabo <msvandaszabo@gmail.com>
2024-04-29 11:46:28 +0200

    style: Add Home page images and text

--- petadoption-frontend/petadoption-app/app/page.tsx
+++ petadoption-frontend/petadoption-app/app/page.tsx
@@ -1,11 +1,19 @@
+import GetStarted from "@/components/GetStarted";
+
 export default function Home() {
   return (
-   <div className="wrapper">
-
-
-    <div className="right">
-
+    <>
+    <div className="wrapper">
+      <div className="left"></div>
+      <div className="right">
+      <h1>Find. <strong>Adopt</strong>. Love.</h1>
+      <GetStarted/>
+      </div>
     </div>
-   </div>
+    <div className="advert">
+      <div className="advert-image"></div>
+      <p>Pawfect Pet Guarantee</p>
+    </div>
+    </>
   );
 }
