9a816f8f99837654a271afc821dffaaa779885a7
vanda.szabo <msvandaszabo@gmail.com>
2024-05-15 22:46:43 +0200

    feat: Install prisma

--- petadoption-frontend/petadoption-app/components/SignInWithGoogleBtn.tsx
+++ petadoption-frontend/petadoption-app/components/SignInWithGoogleBtn.tsx
@@ -3,12 +3,13 @@ import { signIn } from "next-auth/react";
 import { useRouter } from "next/router";
 
 const SignInWithGoogleButton = () => {
+    const router = useRouter();
+    
     const handleClick = async () => {
         const result = await signIn('google', { redirect: false });
         if (result?.error) {
             console.error('Sign-in error:', result.error);
         } else {
-            const router = useRouter();
             router.push('/');
         }
     };
--- petadoption-frontend/petadoption-app/lib/auth.ts
+++ petadoption-frontend/petadoption-app/lib/auth.ts
@@ -1,5 +1,6 @@
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authConfig: NextAuthOptions = {
     session: {
@@ -17,6 +18,28 @@ export const authConfig: NextAuthOptions = {
                   response_type: "code"
                 }
               }
+          }),
+          CredentialsProvider({
+            name: "Login",
+            credentials: {
+              email: { label: "Email", type: "email", placeholder: "example@email.com" },
+              password: { label: "Password", type: "password" }
+            },
+            
+            async authorize(credentials, req) {
+              // Add logic here to look up the user from the credentials supplied
+              const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+        
+              if (user) {
+                // Any object returned will be saved in `user` property of the JWT
+                return user
+              } else {
+                // If you return null then an error will be displayed advising the user to check their details.
+                return null
+        
+                // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+              }
+            }
           })
 
     ]
