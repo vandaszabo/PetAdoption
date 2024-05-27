c82d9037fa3080a4317853ffdfabe6fbab0fe84b
vanda.szabo <msvandaszabo@gmail.com>
2024-05-27 21:54:43 +0200

    debug: Correcting variables and dependencies

--- petadoption-frontend/petadoption-app/next.config.mjs
+++ petadoption-frontend/petadoption-app/next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    env: {
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+        DATABASE_URL: process.env.DATABASE_URL
+      },
     images: {
         remotePatterns: [
             {
