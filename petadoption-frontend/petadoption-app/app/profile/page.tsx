803074b78fa7c6ca431fa397f3fa419ff7c1af30
vanda.szabo <msvandaszabo@gmail.com>
2024-05-30 13:17:46 +0200

    style: Change profile page html

--- petadoption-frontend/petadoption-app/app/profile/page.tsx
+++ petadoption-frontend/petadoption-app/app/profile/page.tsx
@@ -2,14 +2,24 @@
 
 import React from 'react'
 import { useSession } from 'next-auth/react';
+import Logout from '@/components/Logout';
+import Image from 'next/image';
 
 const Profile = () => {
     const { data: session } = useSession();
     return (
-        <>
-            <h2>Name: {session?.user?.name}</h2>
-            <h2>Email: {session?.user?.email}</h2>
-        </>
+        <div className='profile'>
+            <div className='profile-left'>
+                {session?.user?.image &&
+                    <Image className='user-image' src={session?.user?.image as string} alt="user" width={100} height={100} />
+                }
+                <div>
+                    <h2>{session?.user?.name}</h2>
+                    <h3>{session?.user?.email}</h3>
+                </div>
+            </div>
+            <Logout />
+        </div>
     )
 }
 
