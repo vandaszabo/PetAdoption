bb872708fab73d66953ddfe3b6555a11d13ef17f
vanda.szabo <msvandaszabo@gmail.com>
2024-06-06 20:47:11 +0200

    style: All pages are sized

--- petadoption-app/app/login/page.tsx
+++ petadoption-app/app/login/page.tsx
@@ -8,12 +8,12 @@ const LoginPage = () => {
         <h1><p>Happiness</p> starts here</h1>
         <div className="login-image"></div>
       </div>
-      <main className="login-right">
+      <div className="login-right">
         <div className='login-logo'>
           <h3>Login</h3>
         </div>
           <LoginForm />
-      </main>
+      </div>
     </div>
   )
 }
--- petadoption-app/app/register/page.tsx
+++ petadoption-app/app/register/page.tsx
@@ -8,12 +8,12 @@ const SignUpPage = () => {
           <h1><p>Happiness</p> starts here</h1>
           <div className="login-image"></div>
         </div>
-        <main className="login-right">
+        <div className="login-right">
           <div className='login-logo'>
             <h3>Create Account</h3>
           </div>
           <RegisterForm />
-        </main>
+        </div>
       </div>
   )
 }
