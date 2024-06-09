9cad5552480698e3ba1f27885f8252b19b744a6d
vanda.szabo <msvandaszabo@gmail.com>
2024-06-09 20:37:05 +0200

    style: Resize login component

--- petadoption-app/app/login/page.tsx
+++ petadoption-app/app/login/page.tsx
@@ -1,15 +1,17 @@
 import LoginForm from '@/components/LoginForm'
 import React from 'react'
+import Image from 'next/image'
 
 const LoginPage = () => {
   return (
     <div className='login-wrapper'>
       <div className="login-left">
         <h1><p>Happiness</p> starts here</h1>
-        <div className="login-image"></div>
+        <Image className="login-image" src='/login-main-image.png' alt="person-with-cat" priority={true} width={500} height={500} />
       </div>
       <div className="login-right">
         <div className='login-logo'>
+        <Image className="login-logo" src='/paw2.png' alt="paw" width={125} height={125} />
           <h3>Login</h3>
         </div>
           <LoginForm />
--- petadoption-app/app/register/page.tsx
+++ petadoption-app/app/register/page.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import RegisterForm from '@/components/RegisterForm'
+import Image from 'next/image'
 
 const SignUpPage = () => {
   return (
       <div className='login-wrapper'>
         <div className="login-left">
-          <h1><p>Happiness</p> starts here</h1>
-          <div className="login-image"></div>
-        </div>
+        <h1><p>Happiness</p> starts here</h1>
+        <Image className="login-image" src='/login-main-image.png' alt="person-with-cat" priority={true} width={500} height={500} />
+      </div>
         <div className="login-right">
-          <div className='login-logo'>
-            <h3>Create Account</h3>
-          </div>
+        <div className='login-logo'>
+        <Image className="login-logo" src='/paw2.png' alt="paw" width={125} height={125} />
+          <h3>Create Account</h3>
+        </div>
           <RegisterForm />
         </div>
       </div>
