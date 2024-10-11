90298a666ce9fd9362796e38d15db1efb6431c10
Vanda Reszkető-Szabó <122362358+vandaszabo@users.noreply.github.com>
2024-10-11 12:35:48 +0200

    Update layout.tsx with GA tracking code

--- petadoption-app/app/layout.tsx
+++ petadoption-app/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "../components/Footer";
 import SessionWrapper from "@/components/SessionWrapper";
+import Script from "next/script"; // Import Next.js Script component
 
 export const metadata: Metadata = {
   title: "Adopt-a-Pal",
@@ -15,6 +16,24 @@ export default function RootLayout({ children }:
   return (
     <SessionWrapper>
       <html lang="en">
+          <head>
+          {/* Add Google Analytics Script */}
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=G-C05HL0TPYF`}
+            strategy="afterInteractive" // Load after page is interactive
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+             window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-C05HL0TPYF', {
+                page_path: window.location.pathname,
+              });
+            `}
+          </Script>
+        </head>
         <body>
           <Navbar />
           <main>
